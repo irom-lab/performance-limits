99e4e6765bfeefec9f0ca2b5265abf015badc57b
May0Mei <zm2074@princeton.edu>
2022-09-21 21:34:13 -0400

    updated Piecewise Linear Results

--- lava-problem/bound_optimize_l.py
+++ lava-problem/bound_optimize_l.py
@@ -3,12 +3,12 @@ from bound_rewards_l import *
 from lava_problem import main as lava_problem
 import scipy.optimize as optimize
 import matplotlib.pyplot as plt
-from multiprocessing import Pool
+import ray
 
 def main(raw_args=None):
     n = 5 # n slices on interval [0,1]
     # s0 = [0]*(2*n) # set initial guesses to be 0
-    s0 = [-1,-1,-1,-1,-1,1,1,1,1,1]
+    s0_vals = [[[-1]*5+[1]*5]*6+[-10, -5, -3, -2, -1.25, -0.83, -0.6, -0.43, -0.35, -0.28]*14]
 
     # Setups for lava problem
     p_correct_vals = np.linspace(0.01, 0.99, 20) 
@@ -38,26 +38,30 @@ def main(raw_args=None):
     #     res = optimize.minimize(bounds,svec,method = 'trust-constr', constraints = cons, callback = callbackF, args = (p_correct), options={'disp':True})
     #     return[res.x, res.fun]
     
-    def minimize(p_correct):
-        res = optimize.minimize(bounds,s0,method = 'trust-constr', constraints = cons, args = (p_correct))
+    @ray.remote
+    def minimize(p_correct, s0):
+        res = optimize.minimize(bounds,s0,method = 'trust-constr', constraints = cons, args = (p_correct),options = {'disp':True})
         return[res.x, res.fun]
 
-    tr = 20
-    opt_results = []
+    ''' Serial computing '''
+    # tr = 20
+    # opt_results = []
     # s0_guesses = [s0]
-    opt_results.append(minimize(p_correct_vals[0],s0)) # first iteration
+    # opt_results.append(minimize(p_correct_vals[0],s0)) # first iteration
     # s0_guesses.append(opt_results[0][0])
 
-    pool = Pool(4)
-    opt_results = zip(*pool.map(minimize, p_correct_vals))
-
     # for i in range(1,tr):
-    #     opt_result = minimize(p_correct_vals[i],s0_guesses[-1])
+    #     #opt_result = minimize(p_correct_vals[i],s0_guesses[-1])
     #     opt_results.append(opt_result)
     #     s0_guesses.append(opt_result[0])
 
-    vec = [opt_results[i][0] for i in range(tr)]
-    val = [opt_results[i][1] for i in range(tr)]
+    ''' Parallel computing '''
+    ray.init()
+    futures = [minimize.remote(p_correct_vals[i], s0_vals[i]) for i in range(20)]
+    opt_results = ray.get(futures)
+    
+    vec = [opt_results[i][0] for i in range(20)]
+    val = [opt_results[i][1] for i in range(20)]
 
     np.savez('results5_0913.npz',slopes=vec,bound_results=val)
 
---
+++ lava-problem/make_plots_l.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from bound_rewards_l import l
+
+def main():
+
+	# Load data
+	data = np.load("results5_0913.npz")
+	p_correct_vals = np.load("lava_problem_optimal_results.npz")['p_correct_vals']
+	slopes = data['slopes']
+	bounds = data['bound_results']
+	n = int(np.shape(slopes)[1]/2)
+
+	k = 1000
+	xt = np.linspace(0,2,k)
+		
+	fig, ax = plt.subplots(5,4)
+	for i in range(5):
+		for j in range(4):
+			svec = slopes[i*4+j]
+			yt = [l(x,n,svec).value for x in xt]
+			ax[i,j].plot(xt,yt)
+			p = str(p_correct_vals[i*4+j])
+			ax[i,j].set_title(p)
+			ax[i,j].set_xlabel('x')
+			ax[i,j].set_ylabel('f(x)')
+
+	plt.savefig('PL_n=5_func.svg', dpi=200)
+
+#################################################################
+
+# Run with command line arguments precisely when called directly
+# (rather than when imported)
+if __name__ == '__main__':
+    main()  
