a3ddc0a7316083bcf59d8e2578193145304d8ada
May0Mei <zm2074@princeton.edu>
2022-10-10 16:09:04 -0400

    Fixed Fano bounds

--- lava-problem/bound_rewards_Fano.py
+++ lava-problem/bound_rewards_Fano.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 # Definition of Fano bound
-def bound_Fano(R_perp, I):
-    return -(I+np.log(2-R_perp))/np.log(R_perp)
+def bound_Fano(C_perp, I):
+    return -(I+np.log(1+C_perp))/np.log(1-C_perp)
 
 def compute_Fano(nx, nu, ny, T, p0, px_x, py_x, R, R0_expected):
 	'''
     One step Fano bound (T=1)
     '''
 	# Compute R_perp
-	R_u = p0[0] * R
+	R_u = p0 @ R
 	Rt_perp = np.max(R_u)
 
     # Compute mutual information
@@ -23,7 +23,7 @@ def compute_Fano(nx, nu, ny, T, p0, px_x, py_x, R, R0_expected):
 			if (np.abs(pyx_0[ii,jj]) > 1e-5):
 				# mutual information using KL divergence
 				I = I + pyx_0[ii,jj]*np.log(pyx_0[ii,jj]/(py_0[ii]*px_0[jj]))
-	bound = bound_Fano(Rt_perp, I)
+	bound = bound_Fano(1-Rt_perp, I)
 	
 	return bound
 
